68517f54ca3b6f909a07b8d1332e36a49ee1860a
comiser <hoseincomiser27@gmail.com>
2024-01-01 19:17:21 +0330

    register admin payment

--- src/shop/apps/order/permisions.py
+++ src/shop/apps/order/permisions.py
@@ -26,10 +26,10 @@ class IsOrderItemByBuyerOrAdmin(BasePermission):
     def has_permission(self, request, view):
         order_id = view.kwargs.get("order_id")
         order = get_object_or_404(Order, id=order_id)
-        return order.buyer == request.user or request.user.is_staff
+        return order.user == request.user or request.user.is_staff
 
     def has_object_permission(self, request, view, obj):
-        return obj.order.buyer == request.user or request.user.is_staff
+        return obj.order.user == request.user or request.user.is_staff
 
 
 class IsOrderByBuyerOrAdmin(BasePermission):
@@ -41,7 +41,7 @@ class IsOrderByBuyerOrAdmin(BasePermission):
         return request.user.is_authenticated is True
 
     def has_object_permission(self, request, view, obj):
-        return obj.buyer == request.user or request.user.is_staff
+        return obj.user == request.user or request.user.is_staff
 
 
 class IsOrderItemPending(BasePermission):
--- src/shop/apps/payment/admin.py
+++ src/shop/apps/payment/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.apps.payment.models import Payment
+
+admin.site.register(Payment)
